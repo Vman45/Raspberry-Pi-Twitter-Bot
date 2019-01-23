3e2b01a9c1a4aef4eb0205da8572e368f630a7f8
Eric Liu <eric__1234@outlook.com>
2019-01-22 22:15:47 -0500

    Minor change

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key        = 't4QC64cWJirErImk8vYgfI4dp'
-consumer_secret     = 'zKejdBZF2naHLzywCn9MKDkBoHVPtZKgmSeLjTdVnDCYJZU8cI'
-access_token        = '821906769660231681-G55JEXSZK5hdXjVf8Q1sZQDsQeF0yRn'
-access_token_secret = '8FFgBdjuTCnXvXm3O9qBnBwgEuPtzwCcmFywV0FFAq1hq'
+consumer_key        = ''
+consumer_secret     = ''
+access_token        = ''
+access_token_secret = ''
